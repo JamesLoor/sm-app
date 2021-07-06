0a71043c0ab724f91b97700aa52f7303dc02fc53
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-07-06 19:38:05 -0300

    Axios, dotenv y service de auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -8,7 +8,7 @@ export const loginService = (user) => {
   if (email && password) {
     const encode = window.btoa(email + ':' + password)
     return axios.post('http://localhost:3500/auth/sign-in', {}, {
-      headers: { 'Authorization': 'Basic ' + encode }
+      headers: { 'Authorization': 'Basic ' + encode, 'Content-type': "application/json" }
     })
   } else {
     return axios.post('http://localhost:3500/auth/sign-in', {})
