88367c3e9f723d01495fe5be48b3b07ee7a2a16d
James <mendozajames02@hotmail.com>
2021-07-10 00:30:27 -0500

    Test

---
+++ src/components/PatientList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PatientListStyled = styled.div`
+  width: 100%;
+  padding: 20px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+`
+export default function PatientList() {
+  return (
+    <PatientListStyled>
+      
+    </PatientListStyled>
+  )
+}
