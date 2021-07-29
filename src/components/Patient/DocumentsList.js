35108063ee3a302de9e3017a6864c6b87ec30a87
DerianPaez <derianpaez01@hotmail.com>
2021-07-29 18:22:33 -0500

    añadiendo los componentes de documentos de pacientes

---
+++ src/components/Patient/DocumentsList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DocumentsListStyled = styled.div`
+  width: 100%;
+  padding: 20px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 12px;
+  .titleListContainer{
+    padding: 8px 20px;
+    background-color: #093B32;
+    border-radius: 20px;
+  }
+  .titleList{
+    display: grid;
+    grid-template-columns: 1fr 1fr 2fr 120px;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+  }
+`
+
+export default function DocumentsList(){
+  return (
+    <DocumentsListStyled>
+      <div className="titleListContainer">
+        <ul className="titleList">
+          <li>Fecha</li>
+          <li>Tipo</li>
+          <li>Nombre</li>
+        </ul>
+      </div>
+    </DocumentsListStyled>
+  )
+}
--- src/components/Patient/PatientDocuments.js
+++ src/components/Patient/PatientDocuments.js
@@ -2,18 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import PatientBasicInformation from './PatientBasicInformation'
 import PatientHeader from './PatientHeader'
+import DocumentsList from './DocumentsList'
 
 const PatientDocumentsStyled = styled.div`
-    display: grid;
-    grid-template-columns: 240px 1fr;
-    gap: 40px;
+  display: grid;
+  grid-template-columns: 240px 1fr;
+  gap: 40px;
 `
 export default function PatientDocuments() {
   return (
     <PatientDocumentsStyled>
       <PatientBasicInformation/>
       <div className='patientDocumentsContainer'>
-        <PatientHeader/>
+        <PatientHeader title="Documentos Adjuntos"/>
+        <DocumentsList/>
       </div>
     </PatientDocumentsStyled>
   )
