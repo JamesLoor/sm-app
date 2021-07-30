16e511384b7f59a8a08745e1590c4722ae63038b
James <mendozajames02@hotmail.com>
2021-07-30 11:21:11 -0500

    Eliminacion de comentarios

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -3,22 +3,8 @@ import { useState } from 'react'
 const useModal = () => {
   const [isModalOpen, setModalOpen] = useState(false)
 
-  /**
-   * Funcion to open the modal
-   * isOpen = true
-   */
   const openModal = () => setModalOpen(true)
-
-  /**
-   * Funcion to close the modal
-   * isOpen = false
-   */
   const closeModal = () => setModalOpen(false)
-
-  /**
-   * Function to toggle the modal
-   * isOpen = !isOpen
-   */
   const toggleModal = () => setModalOpen((c) => !c)
 
   return [isModalOpen, setModalOpen, openModal, closeModal, toggleModal]
