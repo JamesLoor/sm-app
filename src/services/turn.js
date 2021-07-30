a9b55c9c288964dc9bcdc0aea16e2a51e01580f5
Nicolas-alt <a10100105555@gmail.com>
2021-07-29 21:29:45 -0500

    refactor: turn.js

--- src/services/turn.js
+++ src/services/turn.js
@@ -1,31 +1,31 @@
 import { genService } from '../utils/genService'
-const { REACT_APP_API_URL } = process.env
+
+const baseUrl = process.env.REACT_APP_API_URL
 
 export const getTurns = (token, amount, page) => {
-  let url = REACT_APP_API_URL + `/turn/find/${amount}/${page}`
+  let url = `${baseUrl}/turn/find/${amount}/${page}`
   if (page) {
-    url += '/' + page
+    url += `/${page}`
   }
   return genService(url, 'get', token, undefined)
 }
 
 export const getTurn = (token, turnID) => {
-  let url = REACT_APP_API_URL + `/turn/${turnID}`
+  const url = `${baseUrl}/turn/${turnID}`
   return genService(url, 'get', token, undefined)
 }
 
 export const saveTurn = (token, turn) => {
-  const url = REACT_APP_API_URL + '/turn'
+  const url = `${baseUrl}/turn`
   return genService(url, 'post', token, turn)
 }
 
 export const updateTurn = (token, turnID, turnUpdate) => {
-  const url = REACT_APP_API_URL + '/turn/' + turnID
+  const url = `${baseUrl}/turn/${turnID}`
   return genService(url, 'put', token, turnUpdate)
 }
 
 export const deleteTurn = (token, turnID) => {
-  const url = REACT_APP_API_URL + '/turn/' + turnID
+  const url = `${baseUrl}/turn/${turnID}`
   return genService(url, 'delete', token, undefined)
 }
-  
